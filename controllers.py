5fece6081dc754fa80635eed5bbf3f17ebaae490
k4rimDev <kenanovkenan299@gmail.com>
2023-08-26 19:20:42 +0400

    :sparkles: Change crud

--- controllers.py
+++ controllers.py
@@ -1,16 +1,117 @@
-from flask import render_template
+from flask import render_template, request
 
 from app import app
 from models import Students
+from extensions import db
+
+from sqlalchemy import or_
 
 
 @app.route("/students")
 def students():
-    students = Students.query.filter_by(status="active")
-    return render_template("students.html", students=students)
+    student_count:int = None
+    q:str = None
+    students = Students.query.order_by(Students.name.desc())
+    if request.args.get("q"):
+        q = request.args.get("q")
+        q = "%{q}%".format(q=q)
+        students = Students.query.filter(or_(Students.name.like(q), 
+                                             Students.surname.like(q), 
+                                             Students.bio.like(q), 
+                                             Students.id > 24)).all()
+        
+
+        student_count = len(students)
+        q = q[1:len(q) - 1]
+    return render_template("students.html", students=students, 
+                                            student_count=student_count, 
+                                            q=q)
     
 
 @app.route("/students/<int:id>")
 def student_detail(id):
-    student = Students.query.get(id)
+    student = db.get_or_404(Students, id)
+    
+    # student.view_count += 1
+    # db.session.commit()
+    
     return render_template("student_detail.html", student=student)
+
+
+@app.route("/students/create", methods=['GET', 'POST'])
+def create_student():
+
+    message: str = None
+
+    if request.method == "POST":
+        student = Students(
+            name = request.form["name"],
+            surname = request.form["surname"],
+            gender = request.form["gender"],
+            status = request.form["status"],
+            bio = request.form["bio"],
+        )
+        student.save()
+
+        message = "User is created with successfully!"
+
+    return render_template("create_student.html", message = message)
+
+
+@app.route("/students/update/<int:id>", methods=['GET', 'POST'])
+def update_student(id):
+    student = Students.query.filter_by(id=id)
+    message: str = None
+    if request.method == "POST":
+
+        # new_student = Students(
+            # name = request.form["name"],
+            # surname = request.form["surname"],
+            # gender = request.form["gender"],
+            # status = request.form["status"],
+            # bio = request.form["bio"],
+        # )
+        # new_student.save()
+
+        # student = new_student
+
+        # student.name = request.form["name"]
+        # student.surname = request.form["surname"]
+        # student.gender = request.form["gender"]
+        # student.status = request.form["status"]
+        # student.bio = request.form["bio"]
+
+        # student.data = {
+        #     "name": request.form["name"],
+        #     "surname": request.form["surname"],
+        #     "gender": request.form["gender"],
+        #     "status": request.form["status"],
+        #     "bio": request.form["bio"],
+        # }
+
+        Students.query.filter_by(id=id).update(dict(name = request.form["name"], 
+                                                    surname = request.form["surname"], 
+                                                    gender = request.form["gender"], 
+                                                    status = request.form["status"], 
+                                                    bio = request.form["bio"],))
+        db.session.commit()
+
+        message = "User is updated with successfully!"
+        return render_template("update_student.html", student=student, message=message)
+
+    return render_template("update_student.html", student=student[0], message=message)
+
+
+@app.route("/students/delete/<int:id>", methods=['GET', 'POST'])
+def delete_student(id):
+    # Method 1
+    # db.session.delete(student)
+    # db.session.commit()
+    message:str = None
+
+    # Method 2
+    if request.method == "POST":
+        Students.query.filter_by(id=id).delete()
+        db.session.commit()
+        message = "User with {id} deleted".format(id=id)
+    return render_template("delete.html", message=message)
--- models.py
+++ models.py
@@ -1,14 +1,21 @@
 from extensions import db
+from datetime import datetime
+
+from sqlalchemy.orm import backref
 
 
 class Students(db.Model):
+
+    __tablename__ = "students"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(15), nullable=False)
     surname = db.Column(db.String(20), nullable=False)
     gender = db.Column(db.String(6), nullable=False)
     status = db.Column(db.String(8), nullable=False)
-    image = db.Column(db.Text, nullable=False, server_default="some image path")
+    image = db.Column(db.Text, nullable=False, default="some image path")
     bio = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
         return self.name
@@ -16,3 +23,14 @@ class Students(db.Model):
     def save(self):
         db.session.add(self)
         db.session.commit()
+
+class Blog(db.Model):
+    
+    __tablename__ = "blog"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(15), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
+    students_id = db.Column(db.Integer, db.ForeignKey('students.id'))
+    students = db.relationship("Students", backref=backref("students", uselist=False))
